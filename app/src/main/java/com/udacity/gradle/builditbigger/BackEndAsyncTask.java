37d99158778aeea2da13544dc00a4b6318cd35b4
manoj2prabhakar <manoj2prabhakar@gmail.com>
2016-10-18 09:11:00 +0530

    Final Project 4

--- app/src/main/java/com/udacity/gradle/builditbigger/BackEndAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/BackEndAsyncTask.java
@@ -23,19 +23,6 @@ public class BackEndAsyncTask extends AsyncTask<Pair<Context, String>, Void, Str
     @Override
     public String doInBackground(Pair<Context, String>...params) {
         if(myApiService == null) {  // Only do this once
-            /*MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });*/
-
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
                     .setRootUrl("https://builditbigge-manoj.appspot.com/_ah/api/");
             myApiService = builder.build();
